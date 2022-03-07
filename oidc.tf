eace0bf3223a8108463391300df0cb64e912b1db
odan <odan3240@gmail.com>
2022-03-07 22:11:25 +0900

    add oidc.tf

---
+++ oidc.tf
@@ -0,0 +1,22 @@
+resource "google_service_account" "this" {
+  project    = var.project_id
+  account_id = "oidc-sandbox"
+}
+
+resource "google_project_iam_member" "this" {
+  project = var.project_id
+  role    = "roles/cloudkms.signerVerifier"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
+
+module "gh_oidc" {
+  source      = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id  = var.project_id
+  pool_id     = "oidc-sandbox"
+  provider_id = "oidc-sandbox-gh-provider"
+  sa_mapping = {
+    (google_service_account.this.account_id) = {
+      sa_name = google_service_account.this.name
+    }
+  }
+}
