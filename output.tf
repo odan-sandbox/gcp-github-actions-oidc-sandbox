aa6316e49ef6824d8912b5243039a44b74583000
odan <odan3240@gmail.com>
2022-03-09 13:32:37 +0900

    add output.tf

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "pool_name" {
+  description = "Pool name"
+  value       = module.gh_oidc.pool_name
+}
+
+output "provider_name" {
+  description = "Provider name"
+  value       = module.gh_oidc.provider_name
+}
